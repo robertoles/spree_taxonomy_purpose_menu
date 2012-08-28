3c667124e19a69752c5af3d001d8945af99eddfe
Robert Oles <robertoles@me.com>
2012-08-28 11:36:04 +0100

    Fixing view if no menu is found

--- app/models/taxon_decorator.rb
+++ app/models/taxon_decorator.rb
@@ -2,4 +2,9 @@ Spree::Taxon.instance_eval do
   def menu
     Spree::Taxonomy.menu.root
   end
+
+  def menu_items
+    return [] unless Spree::Taxonomy.menu
+    Spree::Taxonomy.menu.root.children
+  end
 end
